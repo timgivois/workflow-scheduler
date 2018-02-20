a786f90953e8431ab77ca8614990dd7aafb33502
Tim Givois <tim.givois.mendez@gmail.com>
2018-02-20 17:33:08 -0600

    Add a bypass for simulated workflows

--- workflow/simulator.py
+++ workflow/simulator.py
@@ -1,4 +1,4 @@
 from workflow.workflow import Workflow
 
-w = Workflow(40, [0.2, 0.5, 0.2, 0.1], [[.09, .07, .03], [0.01, .01], [.005]])
+w = Workflow(True, 40, [0.2, 0.4, 0.2, 0.2], [[.05, .07, .03], [0.08, .08], [.1]])
 w.show()
