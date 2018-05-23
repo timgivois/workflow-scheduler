93a816ff912376c93c38ed82d8935b1b8ff18990
Tim Givois <tim.givois.mendez@gmail.com>
2018-05-22 19:16:32 -0500

    Modifiyng main python script

--- src/main.py
+++ src/main.py
@@ -4,14 +4,18 @@ from scheduler.simplistic import Simplistic
 from scheduler.random import Random
 from scheduler.extreme import MaxResource, MinResource
 from scheduler.genetic_algorithm import GeneticScheduler
+import time
 
 
 def run_scheduler(scheduler):
+    start = time.time()
     scheduler.schedule()
     total_time, total_cost = scheduler.run()
+    end = time.time()
 
     print('Total time {0}'.format(total_time))
     print('Total Cost {0}'.format(total_cost))
+    print('Total Time {0}'.format(end - start))
 
 
 sim = Initial_Simulation(50, [0.2, 0.3, 0.2, 0.2, 0.2], [[.05, .07, .03, .03], [.05, .04, .04], [.04, 0.05], [0.01]])
@@ -27,16 +31,17 @@ run_scheduler(minResource)
 
 genetic = GeneticScheduler(0, 0, workflow)
 print('\nGenetic')
-run_scheduler(genetic)
-'''
-time_asked = input('Time: ')
-cost_asked = input('Cost: ')
 
-simplistic = Simplistic(time_asked, cost_asked, workflow) # First approach
+run_scheduler(genetic)
+
+#time_asked = input('Time: ')
+#cost_asked = input('Cost: ')
+
+simplistic = Simplistic(0, 0, workflow) # First approach
 print('\n\nSimple scheduling')
+
 run_scheduler(simplistic)
 
-random = Random(time_asked, cost_asked, workflow)
-print('\n\nRandom scheduling')
-run_scheduler(random)
-'''
+#random = Random(time_asked, cost_asked, workflow)
+#print('\n\nRandom scheduling')
+#run_scheduler(random)
